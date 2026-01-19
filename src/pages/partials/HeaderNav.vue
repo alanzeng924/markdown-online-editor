03d768740008b92e99310e9c47333fa08522a54a
AlanZeng423 <alanzeng423@gmail.com>
2026-01-19 16:29:34 +0800

    🔧 Comment out external links in HeaderNav.vue for future reference

--- src/pages/partials/HeaderNav.vue
+++ src/pages/partials/HeaderNav.vue
@@ -10,7 +10,7 @@
         <strong v-if="!isMobile" class="header-text">{{ titleText }}</strong>
       </a>
       <nav class="button-group">
-        <a
+        <!-- <a
           v-if="!isMobile"
           href="https://wechat.jeffjade.com/"
           class="header-link"
@@ -20,13 +20,13 @@
           <span class="hint--bottom" aria-label="公众号 Markdown 排版">
             <icon class="header-icon" name="wechat" />
           </span>
-        </a>
-        <a href="https://www.niceshare.site/" class="header-link" target="_blank" rel="noopener">
+        </a> -->
+        <!-- <a href="https://www.niceshare.site/" class="header-link" target="_blank" rel="noopener">
           <span class="hint--bottom" aria-label="逍遥自在轩">
             <icon class="header-icon" name="homepage" />
           </span>
-        </a>
-        <a
+        </a> -->
+        <!-- <a
           v-if="!isMobile"
           href="https://x.com/MarshalXuan"
           class="header-link"
@@ -36,8 +36,8 @@
           <span class="hint--bottom" aria-label="X - 轩帅">
             <icon class="header-icon" name="x" />
           </span>
-        </a>
-        <a
+        </a> -->
+        <!-- <a
           v-if="!isMobile"
           href="https://github.com/nicejade"
           class="header-link"
@@ -47,7 +47,7 @@
           <span class="hint--bottom" aria-label="作者 Github">
             <icon class="header-icon" name="github" />
           </span>
-        </a>
+        </a> -->
         <router-link to="/about-arya" class="header-link">
           <span class="hint--bottom" aria-label="关于 Arya">
             <icon class="header-icon" name="document" />
